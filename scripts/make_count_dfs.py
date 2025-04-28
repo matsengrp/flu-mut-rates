f75b9d56ca4ddef1321ce682822e05086556637f
Chris Jennings-Shaffer <chrisjenningsshaffer@gmail.com>
2025-04-28 12:35:29 -0700

    documentation

--- scripts/make_count_dfs.py
+++ scripts/make_count_dfs.py
@@ -10,7 +10,23 @@ from math import ceil
 
 
 class CountsHelper:
-    """...handles calculating and writing out mutation counts and conservation per sites..."""
+    """
+    Helper class for calculating and writing per site mutation counts and conservation.
+
+    Attributes:
+        int_nodes (list): The internal nodes of the tree.
+        key_names (tuple): Column names that also serve as keys for Counters.
+        n_int_nodes (int): The number of internal nodes of the tree.
+        n_nodes (int): The number of nodes in the tree.
+        nodes (list): The nodes of the tree.
+        poss_muts (PossibleMutations): Possible Mutations instance for the data.
+        ref_df (pd.DataFrame) = DataFrame of possible mutations from the reference
+            sequence, along with additonal information.
+        ref_seq (str): The reference sequence.
+        translations (defaultdict): A default dictionary mapping a node id to the codon
+            mutations at the node.
+        tree (bte.MATree): Instance of the tree.
+    """
 
     def __init__(
         self,
@@ -22,6 +38,22 @@ class CountsHelper:
         secondary,
         rates,
     ):
+        """
+        Parameters:
+            tree_path (str): The tree.
+            coding_site_path (str): The coding sites dataframe (nucleotide site to codon
+                and gene).
+            fasta_path (str): The fasta for the reference sequence.
+            gtf_path (str):
+            bad_sites (str): The file listing bad sites for initializing self.poss_muts.
+                Currently this is a dummy file, no known bad sites.
+            secondary (str): The file listing secondary structure predictions for
+                initializing self.poss_muts. Currently this is a dummy file, no known
+                secondary structure.
+            rates (str): The file of rates for different mutation types for initializing
+                self.poss_muts. Currently this is a dummy file, no master rates table.
+        """
+
         self.poss_muts = PossibleMutations(
             fasta_path, coding_site_path, bad_sites, secondary, rates, False
         )
@@ -41,9 +73,20 @@ class CountsHelper:
         self.n_int_nodes = len(self.int_nodes)
 
     def ref_motif(self, s):
+        """Return the 3-mer motif centered at the site. Site indices begin at one."""
         return self.ref_seq[s - 2 : s + 1]
 
     def fails_filters(self, nt_muts, codon_muts):
+        """
+        Returns true if the mutations at a node fail the filter. The filter requires
+        that there are between one and four mutations; there is at most one reversion to
+        the reference sequence; and no two mutations target the same codon.
+
+        Parameters:
+            nt_mut (list): List of nucleotide mutations, string entries like "A1234G".
+            codon_muts (list): List of codon mutations, entries are bte.AAChange
+                objects.
+        """
         fail = len(nt_muts) > 4
         fail |= len(nt_muts) == 0
         fail |= sum(self.ref_seq[int(mut[1:-1]) - 1] == mut[-1] for mut in nt_muts) > 1
@@ -51,6 +94,16 @@ class CountsHelper:
         return fail
 
     def count_mutations_from_parent(self, parent):
+        """
+        Count mutations from the parent node to all of its child nodes. Return four
+        values, which are the number of child nodes that pass the filter;
+        a Counter mapping a tuple of values for self.key_names to the number of times
+        the associating mutation type occured; a Counter mapping a tuple of values for
+        self.key_names to the sum of branch lengths where the mutation type is possible;
+        and a list of parent child pairs, where each entry is a triple with of the
+        parent node id, the parent node sequence, and a list of pairs of child node id
+        and child node sequence.
+        """
         parent_node_haplotype = self.tree.get_haplotype(parent.id)
         p_muts = {int(mut[1:-1]): mut[-1] for mut in parent_node_haplotype}
 
@@ -86,8 +139,11 @@ class CountsHelper:
         return n_filtered, counts_dict, branches_dict, pcps
 
     def count_sites_on_node(self, node):
-        """...return Counter where keys are nucleutide sites or codon sites
-        ...with a mutation at this node and those sites...
+        """
+        Count the sites targetted by mutations on this node. Return two Counters, the
+        first maps a nucleotide site to the number of mutations at that site at this
+        node; the second maps a string identifier of the form "{gene}-{codon_site}" to
+        the number of mutations at that codon site at this node.
         """
         node_haplotype = self.tree.get_haplotype(node.id)
         nt_site_count = Counter([mut[1:-1] for mut in node_haplotype])
@@ -97,9 +153,8 @@ class CountsHelper:
 
     def mut_counters_to_df(self, actual_counts, branch_lengths):
         """
-        ...turn a counter with keys as tuples of self.key_names and values for column name
-        ....
-        ...into a df with columns key names and column name...
+        Return a DataFrame with data from two counters like those returned by
+        self.count_mutations_from_parent.
         """
         zipped = zip(actual_counts.items(), branch_lengths.values())
         data = [(*k, v, w) for (k, v), w in zipped]
@@ -109,9 +164,7 @@ class CountsHelper:
 
     def site_counter_to_df(self, counter, n_filtered):
         """
-        ...turn a counter with keys as site s and values for column name
-        ....
-        ...into a df with columns key names and column name...
+        Return a dataframe with site conservation information from a Counter.
         """
         the_df = DF.from_dict(counter, orient="index")
         the_df.reset_index(inplace=True)
@@ -123,12 +176,17 @@ class CountsHelper:
 
     def compute_conservation_on_tree(self):
         """
-        ...
+        Compute the site conservation along the tree. Return the number of internal
+        nodes passing the filter, a dataframe with nucleotide site conversation, and a
+        dataframe with codon site conservation.
         """
         # Use counters rather than a dataframe, since summing together many.
         n_filtered, nt_site_counter, codon_site_counter = 0, Counter(), Counter()
         t0 = -time()
         for i, node in enumerate(self.int_nodes, 1):
+            if i % 1000 == 0:
+                print(f"processing node {i}; {t0+time():0.1f} seconds")
+
             nt_muts = node.mutations
             codon_muts = self.translations[node.id]
             if self.fails_filters(nt_muts, codon_muts):
@@ -138,8 +196,6 @@ class CountsHelper:
             nt_site_count, codon_site_count = self.count_sites_on_node(node)
             nt_site_counter.update(nt_site_count)
             codon_site_counter.update(codon_site_count)
-            if i % 1000 == 0:
-                print(f"{i} nodes processed in {t0+time():0.1f} seconds")
 
         nt_site_df = self.site_counter_to_df(nt_site_counter, n_filtered)
         codon_site_df = self.site_counter_to_df(codon_site_counter, n_filtered)
@@ -147,14 +203,18 @@ class CountsHelper:
 
     def count_mutations_on_tree(self):
         """
-        # Cycle over nodes and record codon mutation counts
-        ...returns int, Counter, Counter
+        Count the number of mutations along the tree. Return the number of nodes passing
+        the filter, a dataframe of per site nucleotide mutations, and a dataframe of
+        parent child pairs.
         """
         # Use counters rather than a dataframe, since summing together many.
         n_filtered, all_actual_counts, all_branch_lengths = 0, Counter(), Counter()
         all_pcps = []
         t0 = -time()
         for i, node in enumerate(self.int_nodes, 1):
+            if i % 1000 == 0:
+                print(f"processing node {i}; {t0+time():0.1f} seconds")
+
             count, actual_counts, branch_lens, pcps = self.count_mutations_from_parent(
                 node
             )
@@ -164,15 +224,13 @@ class CountsHelper:
             if count != 0:
                 all_pcps.append(pcps)
 
-            if i % 1000 == 0:
-                print(f"{i} nodes processed in {t0+time():0.1f} seconds")
         all_counts_df = self.mut_counters_to_df(all_actual_counts, all_branch_lengths)
         all_pcps_df = self.pcp_list_to_df(all_pcps)
         return n_filtered, all_counts_df, all_pcps_df
 
     def parallel_count_mutations_on_tree(self, processes=8, batch_size=1000):
         """
-        ...
+        ...would be nice, but not there yet...
         """
         raise NotImplementedError(
             "Can't multiprocess with bte tree, need to do extra book keeping."
@@ -199,6 +257,10 @@ class CountsHelper:
         return n_filtered, all_counts_df
 
     def pcp_list_to_df(self, pcps):
+        """
+        Turn a list of pcps, like those returned by self.count_mutations_from_parent,
+        into a DataFrame.
+        """
         mut_dict = lambda muts: {int(mut[1:-1]): mut[-1] for mut in muts}
         child_seq = lambda seq, muts: apply_muts(seq, mut_dict(muts))
         cols = ["parent_name", "child_name", "parent", "child", "branch_length"]
@@ -218,6 +280,8 @@ class CountsHelper:
         codon_conserv_path,
         tree_size_path,
     ):
+        """Calculate various counts and write to file."""
+
         print(f"Number of nodes in tree: {self.n_nodes}")
         print(f"Number of internal nodes in tree: {self.n_int_nodes}")
 
@@ -270,6 +334,8 @@ def write_counts(
 
 
 if __name__ == "__main__":
+    # In the main hook, so the rest of the file can be imported by other files.
+
     rerooted_tree_paths = snakemake.input.rerooted_tree_paths
     coding_site_paths = snakemake.input.coding_site_paths
     fasta_paths = snakemake.input.fasta_paths
